9a29430eff91f678ecd619cfbc2964774b5ef3d6
THargreaves <tim.hargreaves@icloud.com>
2020-12-10 17:18:59 +0000

    Create basic build script

---
+++ build/build.py
@@ -0,0 +1,103 @@
+import os
+import shutil
+
+import docker
+
+
+#### SETUP ####
+
+# Connect to Docker daemon
+client = docker.from_env()
+
+# Parameters
+TO_SKIP = (
+    '.ipynb_checkpoints'
+)
+
+# Verify directory structure
+for d in ('content', 'source'):
+    if not os.path.exists('content'):
+	    raise OSError(f"expected directory {d} in working directory")
+	
+# Remove existing post/image directory
+post_src_dir = os.path.join('source', '_posts')
+img_src_dir = os.path.join('source', 'images')
+for d in (post_src_dir, img_src_dir):
+    if os.path.exists(d):
+	    shutil.rmtree(d)
+    os.mkdir(d)
+
+#### BUILD ####
+
+posts = [post for post in os.listdir('content') if not post in TO_SKIP]
+n = len(posts)
+for i, post in enumerate(posts):
+    print(f"Processing post {i+1} of {n}: {post}")
+
+    # Ignore none post directories
+    if post in TO_SKIP:
+        continue
+
+    # Verify directory structure
+    post_dir = os.path.join('content', post)
+    if not os.path.exists(os.path.join(post_dir, 'Dockerfile')):
+        raise OSError(f"couldn't find Dockerfile for post {post}")
+    if not os.path.exists(os.path.join(post_dir, 'post.ipynb')):
+        raise OSError(f"couldn't find notebook for post {post}")
+    
+    # Remove existing support files
+    post_support_files_dir = os.path.join(post_dir, 'post_files')
+    if os.path.exists(post_support_files_dir):
+	    shutil.rmtree(post_support_files_dir)
+
+    # Build docker image
+    client.images.build(path=post_dir, tag=post)
+    
+    # Execute and convert post notebook
+    con = client.containers.run(
+        image=post,
+        # TODO: run as Python script
+        command=' '.join([
+            'jupyter nbconvert',
+            '--execute',
+            '/home/jovyan/work/post.ipynb',
+            '--MarkdownExporter.preprocessors',
+            '"nbconvert.preprocessors.TagRemovePreprocessor"',
+	        '--TagRemovePreprocessor.remove_cell_tags',
+	        '"remove_cell"',
+	        '--TagRemovePreprocessor.remove_input_tags',
+	        '"remove_input"',
+	        '--TagRemovePreprocessor.remove_single_output_tags',
+	        '"remove_single_output"',
+	        '--TagRemovePreprocessor.remove_all_outputs_tags', 
+	        '"remove_all_output"',
+	        '--to markdown',
+        ]),
+        remove=True,
+        volumes={
+            os.path.join(os.getcwd(), post_dir): {'bind': '/home/jovyan/work',
+                                                  'mode': 'rw'},
+        },
+        detach=True
+    )
+    
+    # Stream conversion logs
+    gen = con.logs(stream=True)
+    while True:
+        try:
+            print(gen.__next__().decode('utf-8').strip())
+        except StopIteration:
+            break
+    
+    # TODO: Make post corrections
+    
+    # Move files
+    shutil.move(os.path.join(post_dir, 'post.md'),
+                os.path.join(post_src_dir, f'{post}.md'))
+    if os.path.exists(post_support_files_dir):
+        os.mkdir(os.path.join(img_src_dir, post))
+        for image in os.listdir(post_support_files_dir):
+            shutil.move(os.path.join(post_support_files_dir, image),
+                        os.path.join(img_src_dir, post, image))
+        shutil.rmtree(post_support_files_dir)
+
